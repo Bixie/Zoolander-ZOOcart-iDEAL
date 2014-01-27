62ef34c94e3d0dc34a268a53677ff850b0f8ef1e
Matthijs Alles <admin@bixie.nl>
2014-01-27 11:20:10 +0100

    cleanup

--- ideal/tables/idealcheckout.php
+++ ideal/tables/idealcheckout.php
@@ -23,31 +23,7 @@ class IdealcheckoutTable extends AppTable {
 
 		$object->created_on = $this->app->date->create('now', $this->app->date->getOffset())->toSql();
 
-		$result = parent::save($object);
-
-		if ($result) {
-			$order = $this->app->zoocart->table->orders->get($object->order_id);
-			$config = $this->app->zoocart->getConfig();
-
-			switch ($object->status) {
-				case 1:
-					$order->state = $config->get('payment_received_orderstate', 2);
-					break;
-				case -1:
-					$order->state = $config->get('payment_pending_orderstate', 3);
-					break;
-				case 0:
-				default:
-					$order->state = $config->get('payment_failed_orderstate', 4);
-					break;
-			}
-			$this->app->zoocart->table->orders->save($order);
-		}
-
-		// trigger save event
-		$this->app->event->dispatcher->notify($this->app->event->create($object, 'payment:saved'));
-
-		return $result;
+		return parent::save($object);
 	}
 
 	public function getByOrder($order_id) {
